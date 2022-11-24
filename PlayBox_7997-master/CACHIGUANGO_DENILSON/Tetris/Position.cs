ab43c1e5983f42dee0a7f82a276db0700e8c8f53
Denil1406 <118992426+Denil1406@users.noreply.github.com>
2022-11-24 11:23:29 -0500

    NRC:7997 v1

---
+++ PlayBox_7997-master/CACHIGUANGO_DENILSON/Tetris/Position.cs
@@ -0,0 +1,17 @@
+﻿namespace Tetris
+{
+    public class Position
+    {
+        //Para representar un posición o celda en una cuadr{icula
+        //Almacenar una fila y una columna
+        //line1
+        public int Ron { get; set; }
+        public int Colum { get; set; }
+        //Agregar un construcor simple
+        public Position(int row, int column)
+        {
+            Ron = row ;
+            Colum = Colum;
+        }
+    }
+}
